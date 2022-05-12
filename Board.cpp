35f25046a8cf98059e93bf57563e0f21b7c3db95
d3daa <maxgonick@gmail.com>
2022-05-11 23:57:19 -0700

    Initial commit

---
+++ Board.cpp
@@ -0,0 +1,141 @@
+#include "Board.h"
+#include "Game.h"
+#include "globals.h"
+#include <iostream>
+
+using namespace std;
+
+class BoardImpl
+{
+  public:
+    BoardImpl(const Game& g);
+    void clear();
+    void block();
+    void unblock();
+    bool placeShip(Point topOrLeft, int shipId, Direction dir);
+    bool unplaceShip(Point topOrLeft, int shipId, Direction dir);
+    void display(bool shotsOnly) const;
+    bool attack(Point p, bool& shotHit, bool& shipDestroyed, int& shipId);
+    bool allShipsDestroyed() const;
+
+  private:
+      // TODO:  Decide what private members you need.  Here's one that's likely
+      //        to be useful:
+      int m_Cols;
+      int m_Rows;
+    const Game& m_game;
+    char boardMatrix[][10];
+};
+
+BoardImpl::BoardImpl(const Game& g)
+ : m_game(g)
+{
+    m_Cols = m_game.cols();
+    m_Rows = m_game.rows();
+    or (int i = 0; i < m_Cols; ++i) {
+        for (int j = 0; j < m_Rows; ++j) {
+            boardMatrix[i][j] == '.';
+        }
+    }
+    // This compiles, but may not be correct
+}
+
+void BoardImpl::clear()
+{
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < 10; ++j) {
+            boardMatrix[i][j] == '.';
+        }
+    }
+    // This compiles, but may not be correct
+}
+
+void BoardImpl::block()
+{
+    // TODO:  Replace this with code to block half of the cells on the board
+}
+
+void BoardImpl::unblock()
+{
+    // TODO:  Replace this with code to unblock all blocked cells
+}
+
+bool BoardImpl::placeShip(Point topOrLeft, int shipId, Direction dir)
+{
+    return false; // This compiles, but may not be correct
+}
+
+bool BoardImpl::unplaceShip(Point topOrLeft, int shipId, Direction dir)
+{
+    return false; // This compiles, but may not be correct
+}
+
+void BoardImpl::display(bool shotsOnly) const
+{
+    // This compiles, but may not be correct
+}
+
+bool BoardImpl::attack(Point p, bool& shotHit, bool& shipDestroyed, int& shipId)
+{
+    return false; // This compiles, but may not be correct
+}
+
+bool BoardImpl::allShipsDestroyed() const
+{
+    return false; // This compiles, but may not be correct
+}
+
+//******************** Board functions ********************************
+
+// These functions simply delegate to BoardImpl's functions.
+// You probably don't want to change any of this code.
+
+Board::Board(const Game& g)
+{
+    m_impl = new BoardImpl(g);
+}
+
+Board::~Board()
+{
+    delete m_impl;
+}
+
+void Board::clear()
+{
+    m_impl->clear();
+}
+
+void Board::block()
+{
+    return m_impl->block();
+}
+
+void Board::unblock()
+{
+    return m_impl->unblock();
+}
+
+bool Board::placeShip(Point topOrLeft, int shipId, Direction dir)
+{
+    return m_impl->placeShip(topOrLeft, shipId, dir);
+}
+
+bool Board::unplaceShip(Point topOrLeft, int shipId, Direction dir)
+{
+    return m_impl->unplaceShip(topOrLeft, shipId, dir);
+}
+
+void Board::display(bool shotsOnly) const
+{
+    m_impl->display(shotsOnly);
+}
+
+bool Board::attack(Point p, bool& shotHit, bool& shipDestroyed, int& shipId)
+{
+    return m_impl->attack(p, shotHit, shipDestroyed, shipId);
+}
+
+bool Board::allShipsDestroyed() const
+{
+    return m_impl->allShipsDestroyed();
+}
